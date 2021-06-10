4cbb0c0fc95c3d3616ef222683c46560ac92c6bf
BenjaminWilliamAtkins <benjaminatkins@gmx.de>
2021-06-10 12:25:11 +0200

    - Batkins - first commit

---
+++ php/login.php
@@ -0,0 +1,21 @@
+<?php
+include 'main.php';
+include 'dataBaseConnector.php'
+
+class login
+{
+
+    $dataBaseConnector = new dataBaseConnector();
+
+    function checkPassword( $password )
+    {
+        $isCorrect = false;
+        $this->essbar = $essbar;
+        $this->farbe = $farbe;
+        return 
+    }
+
+}
+
+
+?>
